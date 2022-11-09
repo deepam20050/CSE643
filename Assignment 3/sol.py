23e841bde9adbe454a3dae91b1ab9cbf064189e3
Deepam Sarmah <deepam20050@iiitd.ac.in>
2022-11-09 14:12:23 +0530

    nearing completion

--- Assignment 3/sol.py
+++ Assignment 3/sol.py
@@ -26,24 +26,115 @@ with ruleset('courses'):
   @when_all((m.count >= 3) & (m.cgpa >= 8) & (m.work == 0))
   def mlpath(c):
     c.assert_fact('interest', {'work': 'ml-engineer', 'cgpa': m.cgpa})
+    c.assert_fact({'field': 'ML', 'probability': 90})
 
   @when_all((m.count >= 3) & (m.cgpa >= 7) & (m.work == 0))
   def sdepath(c):
     c.assert_fact('interest', {'work': 'sde', 'cgpa': m.cgpa})
+    c.assert_fact({'field': 'SDE', 'probability': 80})
 
   @when_all((m.count >= 5) & (m.cgpa >= 9) & (m.work == 1))
   def researchpath(c):
     c.assert_fact('interest', {'work': 'research', 'cgpa': m.cgpa})
 
-  @when_all((m.count >= ))
+  @when_all((m.count >= 3) & (m.cgpa >= 9) & (m.work == 2))
+  def uipath(c):
+    c.assert_fact('interest', {'work': 'ui', 'cgpa': m.cgpa})
+  
+  @when_all((m.count >= 3) & (m.cgpa < 8) & (m.work == 2))
+  def gamepath(c):
+    c.assert_fact('interest', {'work': 'game', 'cgpa' : m.cgpa})
+  
+  @when_all((m.count >= 3) & (m.cgpa < 9) & (m.cgpa > 8) & (m.work == 2))
+  def graphicpath(c):
+    c.assert_fact('interest', {'work': 'graphic', 'cgpa' : m.cgpa})
+
+  @when_all(+m.field)
+  def output(c):
+    print('Fact : {0} {1}'.format(c.m.field, c.m.probability))
 
 # {'work', 'cgpa'}
 with ruleset('interest'):
-  pass
+  @when_all((m.work == 'ml-engineer') & (m.cgpa >= 8))
+  def go_ml(c):
+    c.assert_fact('career', {'option': 1})
+  
+  @when_all((m.work == 'ml-engineer') & (m.cgpa >= 7))
+  def go_sde(c):
+    c.assert_fact('career', {'option': 0})
+  
+  @when_all((m.work == 'research') & (m.cgpa >= 9))
+  def goresearch(c):
+    c.assert_fact('career', {'option': 3})
+  
+  @when_all((m.work == 'research') & (m.cgpa >= 7))
+  def go_research_ml(c):
+    c.assert_fact('career', {'option': 1})
 
-# {'cgpa'}
+  @when_all((m.work == 'startup') & (m.cgpa >= 9))
+  def go_startup(c):
+    c.assert_fact('career', {'option': 3})
+  
+  @when_all((m.work == 'startup') & (m.cgpa >= 8))
+  def go_startup(c):
+    c.assert_fact('career', {'option': 0})
+  
+  @when_all((m.work == 'ui') & (m.cgpa >= 8))
+  def go_ui(c):
+    c.assert_fact('career', {'option': 5})
+  
+  @when_all((m.work == 'ui') & (m.cgpa >= 7))
+  def go_ui_graphic(c):
+    c.assert_fact('career', {'option': 4})
+  
+  @when_all((m.work == 'game'))
+  def go_game(c):
+    c.assert_fact('career', {'option': 6})
+  
+  @when_all((m.cgpa < 7))
+  def go_intern(c):
+    c.assert_fact('career', {'option': 7})
+
+# {'occ', 'cgpa'}
 with ruleset('career'):
-  pass
+  @when_all(m.option == 0)
+  def sde(c):
+    c.assert_fact({'occ': 'Software Engineer'})
+  
+  @when_all(m.option == 1)
+  def ml(c):
+    c.assert_fact({'occ': 'ML Engineer'})
+  
+  @when_all(m.option == 2)
+  def research(c):
+    c.assert_fact({'occ': 'Research'})
+
+  @when_all(m.option == 3)
+  def startup(c):
+    c.assert_fact({'occ': 'Entrepreneurship'})
+
+  @when_all(m.option == 4)
+  def graphic(c):
+    c.assert_fact({'occ': 'Graphic Designer'})
+  
+  @when_all(m.option == 5)
+  def ui(c):
+    c.assert_fact({'occ': 'UI/UX Designer'})
+  
+  @when_all(m.option == 6)
+  def game(c):
+    c.assert_fact({'occ': 'Game Developer'})
+  
+  @when_all(m.option == 7)
+  def intern(c):
+    c.assert_fact({'occ': 'Internship'})
+  
+  @when_all(+m.occ)
+  def output(c):
+    print('Career suggested: {0}'.format(c.m.occ))
+
+def get_common (listA, listB):
+  return len(set(listA) & set(listB))
 
 if __name__ == "__main__":
   cgpa = int(input("Enter CGPA: "))
--- Assignment 3/test8b-rules.py
+++ Assignment 3/test8b-rules.py
@@ -3,18 +3,66 @@
 #
 from durable.lang import *
 
+with ruleset('interests'):
+    # will be triggered by 'interests' facts
+    @when_all((m.area == 'human-like-conversations') & (m.type == 'notheory') )
+    def hci(c):
+        c.assert_fact('skills', { 'field': 'probability' })
+        c.assert_fact({ 'subject': 'choose', 'predicate': 'elective', 'object': 'natural language processing' })
+        
+    @when_all((m.area == 'human-like-conversations') & (m.type == 'theory') )
+    def hci(c):
+        c.assert_fact('skills', { 'field': 'probability' })
+        c.assert_fact({ 'subject': 'choose', 'predicate': 'elective', 'object': 'natural language processing' })
+        c.assert_fact('preferences', { 'type': 'nltheory' })
+
+    @when_all((m.area == 'human-like-interactions') & (m.type == 'theory') )
+    def rob(c):
+        c.assert_fact('skills', { 'field': 'ai-ml' })
+        c.assert_fact({ 'subject': 'choose', 'predicate': 'elective', 'object': 'robotics' })
+        c.assert_fact('preferences', { 'type': 'mltheory' })
+
+    @when_all((m.area == 'human-like-interactions') & (m.type == 'notheory') )
+    def rob(c):
+        c.assert_fact('skills', { 'field': 'ai-ml' })
+        c.assert_fact({ 'subject': 'choose', 'predicate': 'elective', 'object': 'robotics' })
+
+    @when_all(+m.subject)
+    def output(c):
+        print('Fact: {0} {1} {2}'.format(c.m.subject, c.m.predicate, c.m.object))
+
+
+with ruleset('skills'):
+    @when_all((m.field == 'probability'))
+    def mathc(d):
+        d.assert_fact({ 'subject': 'take Probability and Statistics course' })
+
+    @when_all((m.field == 'ai-ml'))
+    def mathc(d):
+        d.assert_fact({ 'subject': 'take AI course' })
+        d.assert_fact({ 'subject': 'take ML course' })
+        d.assert_fact( {'subject': 'take DL course' })
+
+    @when_all(+m.subject)
+    def output(d):
+        print('Fact: {0}'.format(d.m.subject))
+
 with ruleset('preferences'):
-    @when_all((m.type >= 5))
+    @when_all((m.type == 'mltheory'))
     def mathct(e):
         e.assert_fact({ 'subject': 'take DL theory course'})
+
+    @when_all((m.type == 'nltheory'))
+    def mathct(e):
+        e.assert_fact({ 'subject': 'take NLP foundations course'})
+
 
     @when_all(+m.subject)
     def output(c):
         print('Fact: {0}'.format(c.m.subject))
 
 
-# assert_fact('interests', { 'area': 'human-like-interactions', 'type': 'notheory' })
-assert_fact('preferences', {'type' : 5})
+assert_fact('interests', { 'area': 'human-like-interactions', 'type': 'notheory' })
 
 
 
